b9c3f42804a54decc79662202711055133e27564
ชุติพันธุ์ จิตราภิรมย์ <g434300043@hotmail.com>
2018-12-15 10:45:37 +0700

    HW

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Calculator
+{
+    class Add
+    {
+        public void Plus(Double Count, Double Count1)
+        {
+            Console.WriteLine("{0}+{1}={2}", Count, Count1, Count + Count1);
+
+        }
+    }
+    class Minus
+    {
+        public void Minu(Double Count, Double Count1)
+        {
+            Console.WriteLine("{0}-{1}={2}", Count, Count1, Count - Count1);
+
+        }
+    }
+    class Multiply
+    {
+        public void Mul(Double Count, Double Count1)
+        {
+            Console.WriteLine("{0}*{1}={2}", Count, Count1, Count * Count1);
+
+        }
+    }
+    class Divide
+    {
+        public void Div(Double Count, Double Count1)
+        {
+            Console.WriteLine("{0}/{1}={2}", Count, Count1, Count / Count1);
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Calculator;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = "\nWelcome to Program Calculator Number\n" +
+                "this program work with 3 or 4 Arguments.\n" +
+                "###################################################################\n" +
+                "Argument 1 = Mode(1.Add 2.Minus 3.Multiply 4.Divide)\n" +
+                "Argument 2 = Count Number\n" +
+                "Argument 3 = Count Number1\n" +
+                "###################################################################\n";
+
+            if (args.Length != 3)
+            {
+                Console.WriteLine("{0}", menu);
+            }
+            else
+            {
+                if (Convert.ToInt32(args[0]) == 1)
+                {
+                    Add CountNB = new Add();
+                    CountNB.Plus(Convert.ToDouble(args[1]), Convert.ToDouble(args[2]));
+                }
+                else if (Convert.ToInt32(args[0]) == 2)
+                {
+                    Minus CountNB = new Minus();
+                    CountNB.Minu(Convert.ToDouble(args[1]), Convert.ToDouble(args[2]));
+                }
+                else if (Convert.ToInt32(args[0]) == 3)
+                {
+                    Multiply CountNB = new Multiply();
+                    CountNB.Mul(Convert.ToDouble(args[1]), Convert.ToDouble(args[2]));
+                }
+                else
+                {
+                    Divide CountNB = new Divide();
+                    CountNB.Div(Convert.ToDouble(args[1]), Convert.ToDouble(args[2]));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
